d5df7174e195a445b12fdab8e1f8be52232b56a6
pratikg144 <prateekg508@gmail.com>
2025-10-30 01:40:01 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#f5d76e',
+        'gold-dark': '#cfb04a'
+      },
+    },
+  },
+  plugins: [],
+};
